b120a909311e6439b2f5ec69706c2bd03f30bf35
Kevin Simper <kevin.simper@gmail.com>
2018-08-16 12:03:00 +0200

    Better way to find url

--- components/Upcoming/index.js
+++ components/Upcoming/index.js
@@ -27,7 +27,7 @@ class FetchEvents extends React.Component {
           const start = event[1][1][3];
           const end = event[1][2][3];
           const title = event[1][4][3];
-          const url = event[1][9][3];
+          const url = event[1].find(i => i[0] === 'url')[3];
           const group = d[0];
           return { start, end, title, url, group };
         });
