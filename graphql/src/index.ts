83169877aef72af212870e067b2e987daaaf10f6
Kevin Simper <kevin.simper@gmail.com>
2020-03-08 19:41:07 +0100

    Add simple express server

--- graphql/src/index.ts
+++ graphql/src/index.ts
@@ -1,1 +1,10 @@
-console.log('Hello');
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Copenhagen.community GraphQL');
+});
+
+const PORT = process.env.PORT || 9000;
+app.listen(PORT, () => console.log('Listening on localhost:' + PORT));
