af454332e082aa39377fd79e0022506c7e74a7e4
Kevin Simper <kevin.simper@gmail.com>
2020-03-10 21:36:35 +0100

    Add query to show upcoming events

--- graphql/src/index.ts
+++ graphql/src/index.ts
@@ -2,9 +2,16 @@ import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import gql from "graphql-tag";
 import { meetups } from "./data/meetups";
+import { fetchEvents } from "./fetch";
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
+  type Event {
+    name: String
+    link: String
+    time: String
+    group: String
+  }
   type Group {
     name: String
     url: String
@@ -16,8 +23,16 @@ const typeDefs = gql`
   type Query {
     hello: String
     groups: [Group]
+    upcomingEvents: [Event]
   }
 `;
+
+type GraphEvent = {
+  name: string;
+  link: string;
+  time: string;
+  group: string;
+};
 
 // Provide resolver functions for your schema fields
 const resolvers = {
@@ -25,6 +40,31 @@ const resolvers = {
     hello: () => "Hello world!",
     groups: () => {
       return meetups;
+    },
+    upcomingEvents: async () => {
+      const groupEvents = await Promise.all(
+        meetups.map(m => {
+          return fetchEvents(m.meetupslug).then(events => {
+            return {
+              group: m.meetupslug,
+              events
+            };
+          });
+        })
+      );
+      const events: GraphEvent[] = groupEvents
+        .map(({ group, events }) => {
+          return events.map(e => {
+            return {
+              name: e.name,
+              link: e.link,
+              time: String(e.time),
+              group
+            };
+          });
+        })
+        .flat();
+      return events;
     }
   }
 };
